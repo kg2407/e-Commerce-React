86342d5fee59ede10db1aea5053881744d1fec6e
kg2407 <iamkg2407@gmail.com>
2021-02-03 12:34:31 +0530

    Added firebase to include the ability to authenticate users into the firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,17 +11,38 @@ const config = {
   appId: '1:584226522115:web:8ac8c30ef9770c382b7a36',
 }
 
-export const createUserProfileDocument = async (userAuth, additionalData) =>{
-  if(!userAuth) return
-}
 firebase.initializeApp(config)
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef
+}
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: 'select_account' })
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
 
 export default firebase
